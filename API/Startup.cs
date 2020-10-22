0906a3fbb005d233e6607acc408d3a01d0331688
Robert Fry <robfry1987@gmail.com>
2020-10-22 21:27:51 +0100

    Client Project added and small API changes.

--- API/Startup.cs
+++ API/Startup.cs
@@ -34,6 +34,9 @@ namespace API
             });
 
             services.AddControllers();
+
+            services.AddCors();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -53,6 +56,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
             app.UseAuthorization();
 
