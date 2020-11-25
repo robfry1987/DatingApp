aaf40ae51a2e6ccdbab25f61bb06bb67a8f3bd14
Robert Fry <robfry1987@gmail.com>
2020-11-25 19:39:07 +0000

    Section 13, lecture 155 completed

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
 import { Member } from '../../_models/member';
+import { Pagination } from '../../_models/pagination';
 import { MembersService } from '../../_services/members.service';
 
 @Component({
@@ -9,11 +9,21 @@ import { MembersService } from '../../_services/members.service';
   styleUrls: ['./member-list.component.css']
 })
 export class MemberListComponent implements OnInit {
-  members$: Observable<Member[]>;
+  members: Member[];
+  pagination: Pagination;
+  pageNumber = 1
+  pageSize = 5;
 
   constructor(private membersService: MembersService) { }
 
   ngOnInit(): void {
-    this.members$ = this.membersService.getMembers();
+    this.loadMembers();
+  }
+
+  loadMembers() {
+    this.membersService.getMembers(this.pageNumber, this.pageSize).subscribe(response => {
+      this.members = response.result;
+      this.pagination = response.pagination;
+    })
   }
 }
